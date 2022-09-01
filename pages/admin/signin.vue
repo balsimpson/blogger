7eb754e994b5d93a21fd7c925078ee6f3aaddcfb
balsipson <thirdi@gmail.com>
2022-09-01 12:30:44 +0530

    Sign in page updated

--- pages/admin/signin.vue
+++ pages/admin/signin.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-black">
-    <div class="w-full max-w-md px-12 py-12 mx-auto bg-black rounded shadow">
+  <div class="flex items-center justify-center min-h-screen bg-white">
+    <div class="w-full max-w-md px-2 py-12 mx-auto bg-transparent sm:px-12">
       <div
-        class="mt-6 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-gray-400 to-gray-800 "
+        class="mt-6 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-teal-400 to-teal-800 "
       >
         Sign in.
-
       </div>
-        <p class="text-gray-500">Create a user in your Firebase Project at <a href="https://console.firebase.com" class="text-yellow-500" target="_blank">Firebase Console</a></p>
+      <p class="mt-2 text-gray-500">
+        Create a user in your Firebase Project at
+        <a
+          href="https://console.firebase.google.com"
+          class="text-teal-500"
+          target="_blank"
+          >Firebase Console </a
+        >to sign in. 
+      </p>
 
       <div class="w-full mx-auto mt-12">
         <form @submit="login" class="space-y-4">
           <div>
             <label
               for="email"
-              class="block text-sm font-medium text-left text-gray-500"
+              class="block text-sm font-bold text-left text-gray-500"
             >
               Email
             </label>
@@ -25,14 +32,14 @@
                 autocomplete="email"
                 required="true"
                 placeholder="john@doe.com"
-                class="w-full px-3 py-1 text-base leading-8 transition-colors duration-200 ease-in-out border rounded outline-none text-stone-200 bg-stone-600 border-stone-600 bg-opacity-20 focus:bg-transparent focus:ring-1 placeholder-stone-700 focus:ring-yellow-500 focus:border-yellow-500 "
+                class="w-full px-3 py-1 text-base leading-8 transition-colors duration-200 ease-in-out border rounded outline-none text-stone-600 bg-stone-600 border-stone-600 bg-opacity-20 focus:bg-transparent focus:ring-1 placeholder-stone-400 focus:ring-teal-500 focus:border-teal-500 "
               />
             </div>
           </div>
           <div class="space-y-1">
             <label
               for="password"
-              class="block text-sm font-medium text-left text-gray-500"
+              class="block text-sm font-bold text-left text-gray-500"
             >
               Password
             </label>
@@ -44,25 +51,38 @@
                 autocomplete="current-password"
                 required="true"
                 placeholder="password"
-                class="w-full px-3 py-1 text-base leading-8 transition-colors duration-200 ease-in-out border rounded outline-none text-stone-200 bg-stone-600 border-stone-600 bg-opacity-20 focus:bg-transparent focus:ring-1 placeholder-stone-700 focus:ring-yellow-500 focus:border-yellow-500 "
+                class="w-full px-3 py-1 text-base leading-8 transition-colors duration-200 ease-in-out border rounded outline-none text-stone-600 bg-stone-600 border-stone-600 bg-opacity-20 focus:bg-transparent focus:ring-1 placeholder-stone-400 focus:ring-teal-500 focus:border-teal-500 "
               />
             </div>
           </div>
 
           <div class="mt-1">
-            <div class="h-2 mb-4 text-sm text-red-500">
+            <div class="h-2 mb-4 text-sm text-center text-red-500">
               {{ loginErrorMsg }}
             </div>
             <button
               @click.prevent="login"
-              class="w-full px-4 py-2 font-bold text-white rounded"
+              class="flex items-center justify-center w-full px-4 py-2 font-bold text-white rounded focus:outline-none focus:ring-teal-800 focus:ring-2 focus:ring-offset-0"
               :class="[
                 emailInput && passwordInput
-                  ? 'bg-stone-600'
-                  : 'bg-stone-800 text-stone-600 pointer-events-none',
+                  ? 'bg-teal-600'
+                  : 'bg-teal-600 opacity-50 text-teal-200 pointer-events-none',
               ]"
             >
-              {{btnText}}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="w-5 h-5 mr-4"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+
+              {{ btnText }}
             </button>
           </div>
         </form>
@@ -73,6 +93,10 @@
 
 <script setup>
 import { signInUser } from "~/composables/useFirebase";
+
+definePageMeta({
+  layout: false,
+});
 
 const loginErrorMsg = ref("");
 const emailInput = ref("");
@@ -91,7 +115,7 @@ const login = async () => {
       router.push("/admin");
     } else {
       loginErrorMsg.value = res;
-      btnText.value = "Sign in"
+      btnText.value = "Sign in";
       setTimeout(() => {
         loginErrorMsg.value = "";
       }, 3000);
